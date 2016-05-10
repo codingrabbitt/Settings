0cf81629ec64787ceb134369ddd4c1067f6ee7c7
David <youchen.du@gmail.com>
2016-05-10 14:20:23 +0800

    [fix] fix git clone to ~/.vim with sudo.

--- vim_settings/InstallVim.sh
+++ vim_settings/InstallVim.sh
@@ -16,10 +16,10 @@ then
     read name
     echo "Your e-mail:\c"
     read email
-    sed -i '' 's/David/'name'/g' ~/.vimrc
-    sed -i '' 's/youchen\.du@gmail\.com/'email'/g' ~/.vimrc
+    sed -i 's/David/'$name'/g' ~/.vimrc
+    sed -i 's/youchen\.du@gmail\.com/'$email'/g' ~/.vimrc
     
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+    sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
     sudo vim +PluginInstall +qall
 else
     echo "Canceled."
