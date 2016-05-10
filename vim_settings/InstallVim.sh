7b7da72705b6ee1cfa3829b5cd19958b74758035
David <youchen.du@gmail.com>
2016-05-10 16:56:19 +0800

    [update] YCM compile requirements.

--- vim_settings/InstallVim.sh
+++ vim_settings/InstallVim.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "This script will overwrite files on this computer."
-echo "Please make sure you have .vimrc file backup."
+echo "Please make sure you have .vimrc and .vim file backup."
 echo "To continue input \"Y\", cancel input \"N\":\c"
 read choice
 if [ "$choice"x = "Y"x ]
 then
     sudo apt-get remove vim vim-common
-    sudo apt-get install build-essential cmake python-dev ctags vim
+    sudo apt-get install build-essential cmake clang python-dev ctags vim
 
     sudo rm -rf ~/.vim
     sudo rm -rf ~/.vimrc
@@ -15,14 +15,24 @@ then
     
     echo "Your name:\c"
     read name
+    if [ "$name"x = x ]
+    then
+        name = "None"
+    fi
+
     echo "Your e-mail:\c"
     read email
+    if [ "$email"x = x ]
+    then email = "None"
+    fi
+    
     sed -i 's/David/'$name'/g' ~/.vimrc
     sed -i 's/youchen\.du@gmail\.com/'$email'/g' ~/.vimrc
     sed -i "s/\(Tlist_Ctags_Cmd\ =\).*/\1 '\/usr\/bin\/ctags'/g" .vimrc
     
     sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
     sudo vim +PluginInstall +qall
+    sudo ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 else
     echo "Canceled."
 fi
